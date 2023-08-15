9cfeb9fc77cb615439a21c66f94f536453fc3ab9
RajaPolavarapu <satyaraj@live.com>
2023-08-15 13:10:17 +0530

    Updated with no loop

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Button from "./Button";
 function BGVideo() {
   return (
     <div className="video-container">
-      <video src={Video} type="video/mp4" autoPlay loop muted className='video' />
+      <video src={Video} type="video/mp4" autoPlay muted className='video' />
       <div className='rsvp-container'>
       <button className='rsvp'>
         <a href='tel:+919441841403' className='tel'>Mobile: +919441841403</a>
