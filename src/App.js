d5f9b4e1c1037556ebe4c6c25839c3d29ffe66de
Raja Polavarapu <satyaraj@live.com>
2023-08-22 18:18:04 +0530

    Updated Video Source

--- src/App.js
+++ src/App.js
@@ -7,21 +7,23 @@ function BGVideo() {
   //Updated Index File with new image 
   return (
     <div className="video-container">
-      <video src={Video} type="video/mp4" autoPlay className='video' />
+      <video loop autoplay="" muted className='video'>
+        <source src={Video} type="video/mp4" />
+      </video>
       <div className='rsvp-container'>
-      <button className='rsvp'>
-        <a href='tel:+919441841403' className='tel'>Mobile: +919441841403</a>
-      </button>
+        <button className='rsvp'>
+          <a href='tel:+919441841403' className='tel'>Mobile: +919441841403</a>
+        </button>
       </div>
       <div className="grid-container">
         <div className="grid-item">
           <Button title={"Marriage Venue Directions"} type="marriage" />
         </div>
         <div className="grid-item">
-         <Button title="Reception Venue Directions" type="reception" />
-          </div>
+          <Button title="Reception Venue Directions" type="reception" />
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
 export default BGVideo;
